937b86020f2c1176c1b0a0fbdf2a66d290f83b52
nooblearner78 <136461377+nooblearner78@users.noreply.github.com>
2024-05-27 23:40:52 +0530

    Update Assignment_1_Seers Swikrit Minz.py

--- Assignment_1_Seers Swikrit Minz.py
+++ Assignment_1_Seers Swikrit Minz.py
@@ -1,20 +1,34 @@
+from matplotlib import pyplot as plt
 import numpy as np
-import cv2 
- 
-img = cv2.imread('E:\PROG\ROBOTICS SUMMER PROJECT\ten.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+import os
+import cv2
 
-img1 =  cv2.GaussianBlur(img, (3, 3), 0)
- 
-circles = cv2.HoughCircles(img1,cv2.HOUGH_GRADIENT,0.5,10,
- param1=45,param2=50,minRadius=80,maxRadius=150)
- 
+#TAKING INPUT IMAGE
+image = cv2.imread('INPUT_IMAGE')
+
+#CONVERTING BGR TO RGB
+re_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+#CONVERTING RGB TO GRAYSCALE
+gray = cv2.cvtColor(re_image, cv2.COLOR_RGB2GRAY)
+
+#APPLYING GAUSSIAN FILTER
+img1 =  cv2.GaussianBlur(gray, (3, 3), 1.8)
+
+#APPLYING CANNY EDGE DETECTION
+edges = cv2.Canny(img1, 50, 100)
+
+#APPLYING HOUGH TRANSFROM FOR CIRCLES
+circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1.2,60,
+ param1=45,param2=60,minRadius=10,maxRadius=170)
+
+#CONVERTING FLOATING POINT TO INT
 circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
- # draw the circle
- cv2.circle(img1,(i[0],i[1]),i[2],(0,255,0),2)
 
- 
-cv2.imshow('detected circles',img1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for i in circles[0,:]:
+ #DRAWING CIRCLES
+ cv2.circle(re_image,(i[0],i[1]),i[2],(255,0,0),2)
+
+#IMAGE DISPLAY WITH IDENTIFIED CIRCLES
+plt.imshow(re_image)
+plt.show()
